f54fde572a0cb619eb753acc4d5a771f1d39d4e0
areee <10089872+areee@users.noreply.github.com>
2020-08-05 20:50:44 +0300

    3rd step (event handling & methods)

--- app.js
+++ app.js
@@ -8,5 +8,12 @@ new Vue({
         "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!",
     },
     newNameText: "",
+    guestName: [],
+  },
+  methods: {
+    formSubmitted: function () {
+      this.guestName.push(this.newNameText);
+      this.newNameText = "";
+    },
   },
 });
